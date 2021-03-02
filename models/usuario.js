99569a672862086fd703bd09c350b85054963fd2
joseluisseg05 <joseluistimy@gmail.com>
2021-03-02 15:04:48 -0600

    Creacion de modelo

---
+++ models/usuario.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+const UsuarioSchema = Shema({
+    nombre: {
+        type: String,
+        require: [true, 'El nombre es un campo obligatorio']
+    },
+    correo : {
+        type: String,
+        require: [true, 'El correo es un campo obligatorio'],
+        unique: true
+    },
+    password : {
+        type: String,
+        require: [true, 'La contraseña es un campo obligatorio']
+    },
+    img : {
+        type: String
+    },
+    rol: {
+        type: String,
+        required: true,
+        emun: ['ADMIN_ROLE', 'USER_ROLE']
+    },
+    estado: {
+        typr: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = model('Usuario', UsuarioSchema);
